bf08ad2412df04be920f6dd8cc0b3c7edded6264
volgakurvar <kurvan1112@gmail.com>
2022-10-01 18:42:14 +0900

    init

---
+++ index.ts
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         kks-url-extractor
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://mercury.bbspink.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mozilla.org
+// @grant        none
+// ==/UserScript==
+
+(function () {
+  const text = document.body.innerText;
+})();
